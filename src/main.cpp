6afe3fd3095835ef9f71458082fbb1cc9701a47d
dmitriiVin <145667180+dmitriiVin@users.noreply.github.com>
2026-06-10 14:03:13 +0300

    add choise 3

--- src/main.cpp
+++ src/main.cpp
@@ -10,19 +10,35 @@ int main() {
     int choise;
 
     while (true) {
-        std::cout << "\nВыберите действие (1 - создать юнита, 2 - показать всех юнитов, 0 - выход): \n";
+        std::cout << "\nВыберите действие (1 - создать юнита, 2 - показать всех юнитов, 3 - удалить юнита по индексу, 0 - выход): \n";
         std::cin >> choise;
         if (choise == 1) {
             Unit unit = CreateUnit();
             units.push_back(unit);
-
-        } else if (choise == 2) {
+        }
+        else if (choise == 2) {
             for (const auto &element : units) {
                 PrintUnit(element);
             }
-        } else if (choise == 0) {
+        }
+        else if (choise == 3) {
+            for (int i = 0; i < units.size(); i++) {
+                std::cout << i << " - " << units[i].name << "\n";
+            }
+            int index;
+            std::cout << "\nВведите что хотите удалить по индексу: ";
+            std::cin >> index;
+            if (index < units.size()) {
+                units.erase(units.begin() + index);
+            }
+            for (const auto &element : units) {
+                PrintUnit(element);
+            }
+        }
+        else if (choise == 0) {
             return 0;
-        } else {
+        }
+        else {
             std::cout << "Неизвестная команда!\n";
         }
     }
